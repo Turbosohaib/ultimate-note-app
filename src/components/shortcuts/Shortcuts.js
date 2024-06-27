52071366843ae6935707f43163f5f96d1de93839
muhibkhan2005 <muhib.ullah@connextar.com>
2024-06-27 22:15:27 +0500

    ui changes

--- src/components/shortcuts/Shortcuts.js
+++ src/components/shortcuts/Shortcuts.js
@@ -1,11 +1,7 @@
-
-
-
 export function Shortcuts() {
-    return (
-        <div className="pt-[50px] pl-[30px]">
-            <h1>Shortcuts</h1>
-        </div>
-    );
+  return (
+    <div className="pt-[50px] pl-[30px] text-5xl text-orange-500 font-bold">
+      <h1>Shortcuts</h1>
+    </div>
+  );
 }
-
